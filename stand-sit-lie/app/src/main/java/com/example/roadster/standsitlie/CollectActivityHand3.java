889f8a0f9bd5fd03f0ecbb2559f8ebc676f4e8b1
cd <cd@admin>
2017-04-19 19:27:47 -0400

    added stand-sit-lie application again

---
+++ stand-sit-lie/app/src/main/java/com/example/roadster/standsitlie/CollectActivityHand3.java
@@ -0,0 +1,35 @@
+package com.example.roadster.standsitlie;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+public class CollectActivityHand3 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collect_activity_hand3);
+    }
+
+    public void walkStart(View view){
+
+        String newAct = getIntent().getStringExtra("new");
+
+        //Creates New Intent
+        Intent startNewActivity = new Intent(this, CollectActivityHandFall3.class);
+
+        startNewActivity.putExtra("new", newAct);
+
+        this.finish();
+
+        //Send To New Activity
+        startActivity(startNewActivity);
+
+
+
+    }
+}
---
+++ stand-sit-lie/app/src/main/java/com/example/roadster/standsitlie/CollectActivityPocket1.java
@@ -0,0 +1,35 @@
+package com.example.roadster.standsitlie;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.roadster.standsitlie.R;
+
+public class CollectActivityPocket1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collect_activity_pocket1);
+    }
+
+    public void walkStart(View view){
+
+        String newAct = getIntent().getStringExtra("new");
+
+        //Creates New Intent
+        Intent startNewActivity = new Intent(this, CollectActivityPocketFall1.class);
+
+        startNewActivity.putExtra("new",newAct);
+
+        this.finish();
+
+        //Send To New Activity
+        startActivity(startNewActivity);
+
+
+
+    }
+}
